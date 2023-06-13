0ce0d52f67b053d871d806356635509d231f3b2d
thiago-guerra <thiago.cesa@hotmail.com>
2023-06-12 23:34:26 -0300

    Incluindo navigation stack

--- src/rotas/AppRotas.js
+++ src/rotas/AppRotas.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Home from '../telas/Home';
-import MelhoresProdutores from '../telas/MelhoresProdutores';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import ProdutorRotas from "./ProdutorRotas";
+import MelhoresProdutoresRotas from "../rotas/MelhoresProdutoresRotas";
 
 const Tab = createBottomTabNavigator();
 
 export default function AppRotas() {
     return <NavigationContainer>
-        <Tab.Navigator>
-            <Tab.Screen name='Home' component={Home}></Tab.Screen>
-            <Tab.Screen name='Melhores Produtores' component={MelhoresProdutores}></Tab.Screen>
-        </Tab.Navigator>
+       <Tab.Navigator>
+        <Tab.Screen name="Home" component={ProdutorRotas}/>
+        <Tab.Screen name="Melhores Produtores" component={MelhoresProdutoresRotas} />
+       </Tab.Navigator>
     </NavigationContainer>
 }
---
+++ src/rotas/ProdutorRotas.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Produtor from '../telas/Produtor/Index';
+import Home from "../telas/Home";
+
+const Stack = createNativeStackNavigator();
+
+export default function ProdutorRotas ({ ComponentePrincipal = Home }) {
+    return <Stack.Navigator>
+        <Stack.Screen name="HomeScreen" component={ComponentePrincipal}/>
+        <Stack.Screen name="Produtor" component={Produtor} />
+    </Stack.Navigator>
+}
